6bac3e152253442ac79ebfd9babc17b18de9850f
Muhammad Bilal Ahmad <111138242+bilalattari407@users.noreply.github.com>
2023-08-01 15:42:10 +0500

    Input_Mark_Sheet
    Input_Mark_Sheet

---
+++ script.js
@@ -0,0 +1,194 @@
+
+        function calculateSum1() {
+            // Get the values entered by the user
+            var num1 = parseFloat(document.getElementById('num1').value);
+            var num2 = parseFloat(document.getElementById('num2').value);
+            var num3 = parseFloat(document.getElementById('num3').value);
+          
+            // Check if the inputs are valid numbers
+            if (isNaN(num1) || isNaN(num2) || isNaN(num3)) {
+                document.getElementById('result').innerHTML = "Please enter valid numbers.";
+                return;
+            }
+
+            // Calculate the sum
+            var sum1 = num1 + num2 + num3;
+
+            // Display the result
+            document.getElementById('result').innerHTML = "Total Marks is: " + sum1;
+        }
+
+        function calculateSum2() {
+            // Get the values entered by the user
+            var num4 = parseFloat(document.getElementById('num4').value);
+            var num5 = parseFloat(document.getElementById('num5').value);
+            var num6 = parseFloat(document.getElementById('num6').value);
+          
+            // Check if the inputs are valid numbers
+            if (isNaN(num4) || isNaN(num5) || isNaN(num6)) {
+                document.getElementById('result2').innerHTML = "Please enter valid numbers.";
+                return;
+            }
+
+            // Calculate the sum
+            var sum2 = num4 + num5 + num6;
+
+            // Display the result
+            document.getElementById('result2').innerHTML = "Total Marks is: " + sum2;
+        }
+  
+
+        function calculateSum3() {
+            // Get the values entered by the user
+            var num7 = parseFloat(document.getElementById('num7').value);
+            var num8 = parseFloat(document.getElementById('num8').value);
+            var num9 = parseFloat(document.getElementById('num9').value);
+          
+            // Check if the inputs are valid numbers
+            if (isNaN(num7) || isNaN(num8) || isNaN(num9)) {
+                document.getElementById('result3').innerHTML = "Please enter valid numbers.";
+                return;
+            }
+
+            // Calculate the sum
+            var sum3 = num7 + num8 + num9;
+
+            // Display the result
+            document.getElementById('result3').innerHTML = "Total Marks is: " + sum3;
+        }
+  
+        function calculateSum4() {
+            // Get the values entered by the user
+            var num10 = parseFloat(document.getElementById('num10').value);
+            var num11 = parseFloat(document.getElementById('num11').value);
+            var num12 = parseFloat(document.getElementById('num12').value);
+          
+            // Check if the inputs are valid numbers
+            if (isNaN(num10) || isNaN(num11) || isNaN(num12)) {
+                document.getElementById('result4').innerHTML = "Please enter valid numbers.";
+                return;
+            }
+
+            // Calculate the sum
+            var sum4 = num10 + num11 + num12;
+
+            // Display the result
+            document.getElementById('result4').innerHTML = "Total Marks is: " + sum4;
+        }
+  
+        function calculateSum5() {
+            // Get the values entered by the user
+            var num13 = parseFloat(document.getElementById('num13').value);
+            var num14 = parseFloat(document.getElementById('num14').value);
+            var num15 = parseFloat(document.getElementById('num15').value);
+          
+            // Check if the inputs are valid numbers
+            if (isNaN(num13) || isNaN(num14) || isNaN(num15)) {
+                document.getElementById('result5').innerHTML = "Please enter valid numbers.";
+                return;
+            }
+
+            // Calculate the sum
+            var sum5 = num13 + num14 + num15;
+
+            // Display the result
+            document.getElementById('result5').innerHTML = "Total Marks is: " + sum5;
+        }
+  
+        function calculateSum6() {
+            // Get the values entered by the user
+            var num16 = parseFloat(document.getElementById('num16').value);
+            var num17 = parseFloat(document.getElementById('num17').value);
+            var num18 = parseFloat(document.getElementById('num18').value);
+          
+            // Check if the inputs are valid numbers
+            if (isNaN(num16) || isNaN(num17) || isNaN(num18)) {
+                document.getElementById('result6').innerHTML = "Please enter valid numbers.";
+                return;
+            }
+
+            // Calculate the sum
+            var sum6 = num16 + num17 + num18;
+
+            // Display the result
+            document.getElementById('result6').innerHTML = "Total Marks is: " + sum6;
+        }
+  
+        function calculateSum7() {
+            // Get the values entered by the user
+            var num19 = parseFloat(document.getElementById('num19').value);
+            var num20 = parseFloat(document.getElementById('num20').value);
+            var num21 = parseFloat(document.getElementById('num21').value);
+          
+            // Check if the inputs are valid numbers
+            if (isNaN(num19) || isNaN(num20) || isNaN(num21)) {
+                document.getElementById('result7').innerHTML = "Please enter valid numbers.";
+                return;
+            }
+
+            // Calculate the sum
+            var sum7 = num19 + num20 + num21;
+
+            // Display the result
+            document.getElementById('result7').innerHTML = "Total Marks is: " + sum7;
+        }
+  
+        function calculateSum8() {
+            // Get the values entered by the user
+            var num22 = parseFloat(document.getElementById('num22').value);
+            var num23 = parseFloat(document.getElementById('num23').value);
+            var num24 = parseFloat(document.getElementById('num24').value);
+          
+            // Check if the inputs are valid numbers
+            if (isNaN(num22) || isNaN(num23) || isNaN(num24)) {
+                document.getElementById('result3').innerHTML = "Please enter valid numbers.";
+                return;
+            }
+
+            // Calculate the sum
+            var sum8 = num22 + num23 + num24;
+
+            // Display the result
+            document.getElementById('result8').innerHTML = "Total Marks is: " + sum8;
+        }
+  
+        function calculateSum9() {
+            // Get the values entered by the user
+            var num25 = parseFloat(document.getElementById('num25').value);
+            var num26 = parseFloat(document.getElementById('num26').value);
+            var num27 = parseFloat(document.getElementById('num27').value);
+          
+            // Check if the inputs are valid numbers
+            if (isNaN(num25) || isNaN(num26) || isNaN(num27)) {
+                document.getElementById('result9').innerHTML = "Please enter valid numbers.";
+                return;
+            }
+
+            // Calculate the sum
+            var sum9 = num25 + num26 + num27;
+
+            // Display the result
+            document.getElementById('result9').innerHTML = "Total Marks is: " + sum9;
+        }
+  
+        function calculateSum10() {
+            // Get the values entered by the user
+            var num28 = parseFloat(document.getElementById('num28').value);
+            var num29 = parseFloat(document.getElementById('num29').value);
+            var num30 = parseFloat(document.getElementById('num30').value);
+          
+            // Check if the inputs are valid numbers
+            if (isNaN(num28) || isNaN(num29) || isNaN(num30)) {
+                document.getElementById('result10').innerHTML = "Please enter valid numbers.";
+                return;
+            }
+
+            // Calculate the sum
+            var sum10 = num28 + num29 + num30;
+
+            // Display the result
+            document.getElementById('result10').innerHTML = "Total Marks is: " + sum10;
+        }
+  
+       
+    
